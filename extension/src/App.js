2277c400a6e8d328665353d64cfeedbdfac981a0
mikeguoynes <mikeguoynes@gmail.com>
2021-12-09 15:41:43 -0500

    can close/reopen modal

--- extension/src/App.js
+++ extension/src/App.js
@@ -4,8 +4,8 @@ import { viewCode } from './main';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
+    <div className="modal-backdrop">
+      <header className="modal">
         <button onClick={viewCode}>
           View Code Only
         </button>
